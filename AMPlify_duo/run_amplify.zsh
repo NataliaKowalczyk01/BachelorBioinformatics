220b4a574b8c40cd3b71a60adb9b8cda91f7d92c
NataliaKowalczyk01 <162929979+NataliaKowalczyk01@users.noreply.github.com>
2024-09-17 12:23:17 +0200

    Update run_amplify_ecoli.zsh

--- AMPlify_duo/run_amplify.zsh
+++ AMPlify_duo/run_amplify.zsh
@@ -2,6 +2,7 @@
 python3 src/train_amplify.py \
     -amp_tr ./data_activity/ecoli_active.fa \
     -non_amp_tr ./data_activity/ecoli_inactive.fa \
+    -amp_te ./data_test/union_ecoli_active_test.fa \
+    -non_amp_te ./data_test/union_ecoli_inactive.fa \
     -out_dir ./models_amplify_ecoli \
     -model_name amplify_ecoli
-    
